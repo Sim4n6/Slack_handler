2838b7f3d1da5e3a46f627c7b895618f1a17e0fb
ALJI Mohamed <sim4n6@gmail.com>
2020-11-23 23:16:51 +0100

    bash script to create a disk image with a unique partition that contain files from Govdocs1

--- Scripts/gen-disk-img-specimens.sh
+++ Scripts/gen-disk-img-specimens.sh
@@ -1,112 +1,124 @@
 #!/bin/bash
 #
 # Script to generate disk images containing NTFS specimens
+#
+# USAGE: sudo bash gen-disk-img-specimens.sh di1.raw ../Govdocs1/myThread0
 
-EXIT_SUCCESS=0;
-EXIT_FAILURE=1;
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
 
 # Checks the availability of a binary and exits if not available.
 #
 # Arguments:
 #   a string containing the name of the binary
 #
-assert_availability_binary()
-{
-	local BINARY=$1;
+assert_availability_binary() {
+    local BINARY=$1
 
-	which ${BINARY} > /dev/null 2>&1;
-	if test $? -ne ${EXIT_SUCCESS};
-	then
-		echo "Missing binary: ${BINARY}";
-		echo "";
+    which "${BINARY}" >/dev/null 2>&1
+    if test $? -ne ${EXIT_SUCCESS}; then
+        echo "Missing binary: ${BINARY}"
+        echo ""
 
-		exit ${EXIT_FAILURE};
-	fi
+        exit ${EXIT_FAILURE}
+    fi
 }
 
-assert_availability_binary dd;
-assert_availability_binary mkntfs;
-assert_availability_binary losetup;
-assert_availability_binary parted;
+assert_availability_binary dd
+assert_availability_binary mkntfs
+assert_availability_binary losetup
+assert_availability_binary parted
 
-# exit immediately with a none zero if a command fails.
-set -e;
+# exit immediately with a none zero value if a command fails.
+set -e
 
-# cli arguments 
-TEST_IMG_NAME=$1 # and location eg: di.raw  
-DIR_TO_COPY=$2 # recursively eg: ../Govdocs1/myThread0/
+# cli arguments
+TEST_IMG_NAME=$1 # eg: di.raw
+IMG_WITHOUT_EXT=$(echo "${TEST_IMG_NAME}" |awk -F'.' '{print $1}')
+DIR_TO_COPY=$2   # eg: ../Govdocs1/myThread0
 
 SECTOR_SIZE=512
-CLUSTER_SIZE=1024
-
 
 # Compute the total size of the disk image
-TOTAL_IMG_SIZE=0 
-for f in $(ls ${DIR_TO_COPY})
-do 
-    echo "/$f/ ... ";
-    for fi in $(ls ${DIR_TO_COPY}/${f})
-    do
-        echo "/${f}/${fi} <--";
-        FILE_SIZE=$(du -b ${DIR_TO_COPY}/${f}/${fi} | awk '{print $1}'); # in bytes
-        echo "$FILE_SIZE in bytes";
-        TOTAL_IMG_SIZE=$(( $TOTAL_IMG_SIZE+$FILE_SIZE ));
-    done;
-done;
+compute_total_needed_size() {
 
-# add an upper approximate of NTFS SYSTEM files size. 
-NTFS_SYS_FILES=$((3*1024*1024))
-TOTAL_IMG_SIZE=$(( $TOTAL_IMG_SIZE+$NTFS_SYS_FILES))
-echo "TOTAL IMAGE SIZE: $TOTAL_IMG_SIZE in bytes."
-echo "TOTAL IMAGE SIZE: $(($TOTAL_IMG_SIZE/$SECTOR_SIZE)) in sectors."
-sleep 3
+    #local FILE_SIZE, NTFS_SYS_FILES, TO_ALIGN_PARTITIONS
+    TOTAL_IMG_SIZE=0
+    for f in $(ls ${DIR_TO_COPY}); do
+        echo "/$f/ ... "
+        for fi in $(ls ${DIR_TO_COPY}/${f}); do
+            #echo "/${f}/${fi} <--"
+            FILE_SIZE=$(du -b ${DIR_TO_COPY}/${f}/${fi} | awk '{print $1}') # in bytes
+            #echo "$FILE_SIZE in bytes"
+            TOTAL_IMG_SIZE=$((TOTAL_IMG_SIZE +FILE_SIZE))
+        done
+    done
 
-TO_ALIGN_PARTITIONS=2048 # in sectors
-TOTAL_IMG_SIZE=$(( $TOTAL_IMG_SIZE + $TO_ALIGN_PARTITIONS * $SECTOR_SIZE ))
+    # add an upper approximate of NTFS SYSTEM files size.
+    NTFS_SYS_FILES=$((3 * 1024 * 1024))
+    TOTAL_IMG_SIZE=$((TOTAL_IMG_SIZE + NTFS_SYS_FILES))
+    sleep 3
 
-# define the mount point
-MOUNT_POINT="/mnt/${TEST_IMG_NAME}";
-mkdir -p $MOUNT_POINT
+    TO_ALIGN_PARTITIONS=2048 # in sectors
+    TOTAL_IMG_SIZE=$((TOTAL_IMG_SIZE + TO_ALIGN_PARTITIONS * SECTOR_SIZE))
+}
 
-# the looping device that will be used 
-FREE_DEV=$(losetup --find | awk -F "/" '{print $3}') 
-echo "The free device is ${FREE_DEV}."
-
-# create the TEST_IMG file and make it NTFS formatted
-dd if=/dev/zero of=${TEST_IMG_NAME} bs=${SECTOR_SIZE} count=$(( ${TOTAL_IMG_SIZE}/${SECTOR_SIZE} )) status=progress 2> /dev/null;
-mkntfs -F -q -L "label_${TEST_IMG_NAME}" -s ${SECTOR_SIZE} ${TEST_IMG_NAME};
+compute_total_needed_size
+echo "Disk img size is computed:"
+echo " - TOTAL IMAGE SIZE: ${TOTAL_IMG_SIZE} in bytes."
+echo " - TOTAL IMAGE SIZE: $((TOTAL_IMG_SIZE / SECTOR_SIZE )) in sectors."
 sleep 5
 
-# mount in the free looping device 
-losetup -f ${TEST_IMG_NAME}
+# define the mount point
+MOUNT_POINT="/mnt/${IMG_WITHOUT_EXT}"
+mkdir -p "${MOUNT_POINT}"
 
-# print the devices and mounted dirs
-losetup -a
+# create the TEST_IMG file and make it NTFS formatted
+dd if=/dev/zero of="${TEST_IMG_NAME}" bs="${SECTOR_SIZE}" count=$((TOTAL_IMG_SIZE / SECTOR_SIZE)) status=progress 2>/dev/null
 
-# partition
+# the looping device that will be used
+FREE_DEV=$(losetup --find)
+echo "The free device is: s"
 
-startp1="${TO_ALIGN_PARTITIONS}s"
-endp1="$(( $TOTAL_IMG_SIZE / $SECTOR_SIZE -1))s"
-parted -s /dev/"${FREE_DEV}" mklabel msdos mkpart primary NTFS $startp1 $endp1;
-parted -s /dev/"${FREE_DEV}" print;
+# set up and control the free looping device
+losetup --show "${FREE_DEV}" "${TEST_IMG_NAME}"
 sleep 3
 
-# Copying ....
-for f in $(ls ${DIR_TO_COPY})
-do 
-    echo "/${f}/ ... ";
-    for fi in $(ls ${DIR_TO_COPY}/${f})
-    do
-        echo "/${f}/${fi} <--"
-        cp ${DIR_TO_COPY}/${f}/${fi} ${MOUNT_POINT}/${fi}
-    done;
-done;
+# partition
+startp1="${TO_ALIGN_PARTITIONS}s"
+parted -s "${FREE_DEV}" -- mklabel msdos mkpart primary NTFS $startp1 -1s
+parted -s "${FREE_DEV}" print
+sleep 3
 
-# unmount and delete the looping device 
-losetup -d /dev/${FREE_DEV}
+mkntfs -F -q -L "label_${IMG_WITHOUT_EXT}" -s ${SECTOR_SIZE} "${FREE_DEV}p1"
+
+sleep 3
+mount "${FREE_DEV}p1" "${MOUNT_POINT}"
+
+# print everything
+lsblk -o NAME,FSTYPE,LABEL,UUID,MOUNTPOINT
+
+sleep 3
+# Copying ....
+copy_dir_content() {
+
+    for f in $(ls ${DIR_TO_COPY}); 
+    do
+        for fi in $(ls ${DIR_TO_COPY}/${f}); do
+            cp "${DIR_TO_COPY}/${f}/${fi}" "${MOUNT_POINT}/${fi}"
+        done
+    done
+
+}
+
+copy_dir_content;
+
+# unmount and delete the looping device
+umount "${MOUNT_POINT}"
+losetup --detach "${FREE_DEV}"
 
 # Copy and then Remove the working image file.
-cp ${TEST_IMG_NAME} ../test_data/${TEST_IMG_NAME}
-rm -f ${TEST_IMG_NAME};
+cp "${TEST_IMG_NAME}" "../test_data/${TEST_IMG_NAME}"
+rm -f "${TEST_IMG_NAME}"
 
-exit ${EXIT_SUCCESS};
+exit ${EXIT_SUCCESS}
