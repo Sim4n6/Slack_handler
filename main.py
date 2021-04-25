4f323c5de341629aff4fa5ef1552d52ca2642912
Sim4n6 <sim4n6@gmail.com>
2021-04-25 16:57:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,22 @@
 **************************************************************************************.
 Original version: 1.0 Date: 13/06/2012 by Sokratis Vidros <sokratis.vidros@gmail.com>.
 Current updated version: 1.1 Date 15/10/2020 by ALJI Mohamed <sim4n6@gmail.com>.
+
+    Copyright (C) 2020 - 2021 ALJI Mohamed <sim4n6@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 **************************************************************************************.
 """
 
