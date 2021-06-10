91c27125514329f218685dd9fa519faff2866fe0
ALJI Mohamed <sim4n6@gmail.com>
2021-06-10 21:57:07 +0100

    parametrized some tests to include EWF format

--- tests/test__files_slack_content.py
+++ tests/test__files_slack_content.py
@@ -9,13 +9,14 @@ CWD = Path().cwd()
 TEST_DATA_DIR = CWD.joinpath("test_data")
 SRC_DIR = CWD.joinpath("src")
 
+# FIXME styling ?
 # appending a SRC_DIR path for importing utils module
 sys.path.append(str(SRC_DIR))
 import utils
 
-
-def test__files_slack_nbr(tmpdir):
-    """ assert a correct number of file slacks per each disk img """
+@pytest.mark.parametrize("disk_img, disk_img_type, nbr_file_slacks", [("di1.raw", "raw", 11), ("di3.e01", "ewf", 11)])
+def test__files_slack_nbr(tmpdir, disk_img, disk_img_type, nbr_file_slacks):
+    """assert a correct number of file slacks per each disk img"""
 
     SLACKS_DIR = tmpdir.mkdir("slacks")
 
@@ -23,11 +24,11 @@ def test__files_slack_nbr(tmpdir):
         [
             "python3",
             SRC_DIR.joinpath("main.py"),
-            "-t",
-            "raw",
+            "--type",
+            disk_img_type,
             "--dump",
             SLACKS_DIR,
-            TEST_DATA_DIR.joinpath("di1.raw"),
+            TEST_DATA_DIR.joinpath(disk_img),
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -35,39 +36,38 @@ def test__files_slack_nbr(tmpdir):
     stdoutput, error = proc.communicate()
 
     files_found = [sf for sf in SLACKS_DIR.listdir() if sf.check(file=True)]
-    assert len(files_found) == 11
+    assert len(files_found) == nbr_file_slacks
 
 
-def test__file_slack_fn(tmpdir):
+@pytest.mark.parametrize("disk_img, disk_img_type", [("di1.raw", "raw"), ("di3.e01", "ewf")])
+def test__file_slack_fn(tmpdir, disk_img, disk_img_type):
     """check a specific file slack filename found and format of all files dumped correspond to 'slack--XXXXXdd'"""
-    
+
     SLACKS_DIR = tmpdir.mkdir("slacks")
     proc = subprocess.Popen(
         [
             "python3",
             SRC_DIR.joinpath("main.py"),
-            "-t",
-            "raw",
+            "--type",
+            disk_img_type,
             "--dump",
             SLACKS_DIR,
-            TEST_DATA_DIR.joinpath("di1.raw"),
+            TEST_DATA_DIR.joinpath(disk_img),
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
     stdoutput, error = proc.communicate()
 
-    files_found_fn = [
-        sf.basename for sf in SLACKS_DIR.listdir() if sf.check(file=True)
-    ]
+    files_found_fn = [sf.basename for sf in SLACKS_DIR.listdir() if sf.check(file=True)]
     assert "slack--001961.pdf.dd" in files_found_fn
     for slack_f in files_found_fn:
         assert re.match("^slack--.+dd$", slack_f)
 
-
-def test__file_slack_content(tmpdir):
+@pytest.mark.parametrize("disk_img, disk_img_type", [("di1.raw", "raw"), ("di3.e01", "ewf")])
+def test__file_slack_content(tmpdir, disk_img, disk_img_type):
     """Ensure all computed MD5 of the extracted file slacks are equals to the ones in the results.csv generated report."""
-    
+
     SLACKS_DIR = tmpdir.mkdir("slacks")
     REPORT_DIR = tmpdir.mkdir("report")
     CSV_REPORT = REPORT_DIR.join("results.csv")
@@ -76,12 +76,12 @@ def test__file_slack_content(tmpdir):
             "python3",
             SRC_DIR.joinpath("main.py"),
             "--type",
-            "raw",
+            disk_img_type,
             "--dump",
             SLACKS_DIR,
             "--csv",
             CSV_REPORT,
-            TEST_DATA_DIR.joinpath("di1.raw"),
+            TEST_DATA_DIR.joinpath(disk_img),
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
