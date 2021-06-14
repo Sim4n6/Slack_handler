64014420adc6b67147c9fbb4beba5e7ae34d68ed
Sim4n6 <sim4n6@gmail.com>
2021-06-14 11:58:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 from pathlib import Path
 
-PARENT_DIR = Path().cwd()
-README_PATH = PARENT_DIR.joinpath("README.md")
+CURRENT_DIR = Path().cwd()
+README_PATH = CURRENT_DIR.joinpath("README.md")
 
 with README_PATH.open() as f:
     README = f.read()
 
 setup(
     name="slack_handler",
-    version="v0.2.6",
+    version="v0.2.7",
     description="Python tool to extract File slacks from disk images.",
     url="https://github.com/Sim4n6/Slack_handler",
     long_description=README,
